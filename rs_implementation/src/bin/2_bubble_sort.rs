9cc8172d6c566e44ac5732451f3724b4d49ed273
Hbbolaji <bolaji1050@gmail.com>
2026-09-03 12:24:47 +0100

    practice: implemented bubble sort in rs

--- rs_implementation/src/bin/2_bubble_sort.rs
+++ rs_implementation/src/bin/2_bubble_sort.rs
@@ -1,1 +1,26 @@
-fn main() {}
+fn main() {
+    let mut list = [64, 34, 25, 12, 22, 11, 90, 5];
+    let mut i = 0;
+
+    let n = list.len();
+    while i < n - 1 {
+        let mut is_swapped = false;
+        let mut j = 0;
+        while j < n - i - 1 {
+            let first = list[j];
+            let next = list[j + 1];
+            if first > next {
+                list[j] = next;
+                list[j + 1] = first;
+            }
+            is_swapped = true;
+            j += 1;
+        }
+        if !is_swapped {
+            break;
+        }
+        i += 1;
+    }
+
+    println!("{list:?}")
+}
